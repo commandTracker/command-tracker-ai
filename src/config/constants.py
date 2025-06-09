d22517d735e35fb9e796691ddf85b6c212ba2bd5
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-09 16:40:23 +0900

    refactor: email 내용 메시지 수정

--- src/config/constants.py
+++ src/config/constants.py
@@ -2,6 +2,8 @@ class GCS:
     SIGNED_URL_EXPIRE = 60 * 24
 
 class MESSAGES:
+    class SUCCESS:
+        ANALYZE = "영상 분석을 완료했습니다."
     class ERROR:
         NOT_FOUND_VIDEO = "영상을 찾을 수 없습니다."
         FAILED_ANALYZE = "영상 분석에 실패했습니다."
@@ -11,7 +13,3 @@ class MESSAGES:
         FAILED_CONNECT_CHANNEL = "채널 연결에 실패했습니다."
 
         SERVER_ERROR = "서버 오류가 발생했습니다."
-
-class ANALYZE_RESULT:
-    SUCCESS = "success"
-    FAILED = "failed"

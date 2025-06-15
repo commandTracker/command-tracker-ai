f2f06eba28fa38fbd032b121c01200ed7bf85698
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-16 02:47:37 +0900

    refactor: gcs영상 스트림으로 가져오도록 변경

--- src/gcs/read.py
+++ src/gcs/read.py
@@ -1,14 +1,11 @@
-import os
-
 from config.constants import MESSAGES
 from config.gcs import get_bucket
 
-def get_video(file_name, save_dir):
+def get_video(blob_name):
     try:
         bucket = get_bucket()
-        blob_name = f"edited/{file_name}"
         blob = bucket.blob(blob_name)
 
-        blob.download_to_filename(os.path.join(save_dir, f"{file_name}.mp4"))
+        return blob.open("rb")
     except:
         raise FileNotFoundError(MESSAGES.ERROR.NOT_FOUND_VIDEO)

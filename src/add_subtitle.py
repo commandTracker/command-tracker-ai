cea5d25bf01ca0349f9e212ffd236d6de1102f5b
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-16 02:51:39 +0900

    feat: ffmpeg을 이용하여 영상에 자막 삽입

---
+++ src/add_subtitle.py
@@ -0,0 +1,23 @@
+import ffmpeg
+import io
+from config.constants import MESSAGES
+
+def add_subtitle_to_video(video_bytes, subtitle_path):
+    try:
+        input_video = ffmpeg.input('pipe:0')
+
+        output, _ = (
+            input_video
+            .filter('subtitles', subtitle_path)
+            .output('pipe:1',
+                    vcodec='libvpx-vp9',
+                    acodec='libvorbis',
+                    format='webm',
+                    movflags='faststart')
+            .run(input=video_bytes, capture_stdout=True)
+        )
+
+        return io.BytesIO(output)
+
+    except ffmpeg.Error:
+        raise RuntimeError(MESSAGES.ERROR.FAILED_ADD_SUBTITLE)

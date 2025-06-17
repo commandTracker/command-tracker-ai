6c5714cc34ee104275fc90b0e78e631ec2f594ab
ChaJunWoo18 <wnsdnxla@naver.com>
2025-06-17 16:57:16 +0900

    feat: 멀티 프로세싱으로 consume 병렬처리 구현

--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-import os
+from multiprocessing import Process
 
-from config.rabbitmq import connect
 from rabbitmq.consume import consume_message
+from config.rabbitmq import create_connection
+
+def worker():
+    connection, channel = create_connection()
+
+    channel.basic_qos(prefetch_count=1)
+    consume_message(channel)
+    connection.close()
 
 def main():
-    if not os.path.exists("temp"):
-        os.makedirs("temp")
+    workers = []
 
-    connect()
-    consume_message()
+    for _ in range(3):
+        p = Process(target=worker)
+
+        p.start()
+        workers.append(p)
+
+    for p in workers:
+        p.join()
 
 if __name__ == "__main__":
     main()
